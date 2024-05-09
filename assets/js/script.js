663417598236e6aaad91ca353b9d46f5c0746d57
fvyia <magdayaofrances@gmail.com>
2024-05-09 08:14:38 +0800

    modified: assets/script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -43,3 +43,23 @@ navToggleBtn.addEventListener("click", function () {
   elemToggleFunc(document.body);
 
 });
+
+
+/**
+ * skills toggle
+ */
+
+const toggleBtnBox = document.querySelector("[data-toggle-box]");
+const toggleBtns = document.querySelectorAll("[data-toggle-btn]");
+const skillsBox = document.querySelector("[data-skills-box]");
+
+for (let i = 0; i < toggleBtns.length; i++) {
+  toggleBtns[i].addEventListener("click", function () {
+
+    elemToggleFunc(toggleBtnBox);
+    for (let i = 0; i < toggleBtns.length; i++) { elemToggleFunc(toggleBtns[i]); }
+    elemToggleFunc(skillsBox);
+
+  });
+}
+
